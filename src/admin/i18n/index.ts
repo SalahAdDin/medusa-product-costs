8885b34afeee77d98a7c96cbf4c058ca762ce69e
José Luis <alagunasalahaddin@live.com>
2026-09-21 01:05:27 -0500

    feat: add ar, es and tr licalization files (#15)

--- src/admin/i18n/index.ts
+++ src/admin/i18n/index.ts
@@ -1,11 +1,23 @@
+import ar from "./json/ar.json" with { type: "json" };
 import en from "./json/en.json" with { type: "json" };
+import es from "./json/es.json" with { type: "json" };
 import pl from "./json/pl.json" with { type: "json" };
+import tr from "./json/tr.json" with { type: "json" };
 
 export default {
+  ar: {
+    translation: ar,
+  },
   en: {
     translation: en,
   },
+  es: {
+    translation: es,
+  },
   pl: {
     translation: pl,
+  },
+  tr: {
+    translation: tr,
   },
 };
